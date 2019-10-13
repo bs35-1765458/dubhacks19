d3e55d7c2ac5c3825472a7f4e9901270cf2904ba
Aditi Chauhan <caditi97@gmail.com>
2019-10-13 00:53:39 -0700

    Add files via upload

--- content.js
+++ content.js
@@ -1,9 +1,34 @@
 
-// finds all img tags in the page, store them into an array
-const images = document.getElementsByTagName('img'); 
+
+const images = document.getElementsByTagName('img');
+// var pars = $("p");
+// text = pars[0].innerHTML;
+var arrOfStrings = [];
 for(var i = 0; i < images.length; i++) {
-    //function call comes here on images[i].src
+  $.ajax({
+    type: "POST",
+    url: "/Users/butterchicken/desktop/dubhacks19/mathread.py",
+    data: { param: images[i].src},
+    success: function() {
+      console.log("something");
+      //function call comes here on images[i].src
+      //run_quickstart(images[i].src);
+      var myAudio = new Audio('/Users/butterchicken/desktop/dubhacks19/output.mp3');
+      myAudio.play();
+    }
+  });
 }
+
+// }).done(function( o ) {
+   // finds all img tags in the page, store them into an array
+
+//
+console.log(arrOfStrings);
+
+//const text = re.query.message \\ req.body.message\\'';
+
+
+
 
 // this potentially could be removed
 chrome.runtime.sendMessage({
